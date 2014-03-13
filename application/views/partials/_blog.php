960a76f2bdd1eed8125af21f2badfb242b09a97f
Derrick Roccka <derrickroccka@gmail.com>
2014-03-13 11:47:53 +0100

    blog title put on left

--- application/views/partials/_blog.php
+++ application/views/partials/_blog.php
@@ -11,5 +11,5 @@ preg_match("'<h2 class=\"post-title\">(.*?)</h2>'", $html, $display);
 		.blog-title a{color: #444; text-decoration: none;}
 		.blog-title:hover a{color: #cf7aab;}
 	</style>
-	<p style="font-size: 7rem;text-align:center; margin-top: 15%;color: #444;" class="blog-title"><?php echo $display[1];?></p>
+	<p style="font-size: 7rem;text-align:left; color: #444; max-width: 33%;" class="blog-title">"<?php echo $display[1];?>"</p>
 </section>
