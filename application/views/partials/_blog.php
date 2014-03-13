8e795220d17f8391a87e313c9a359d0d2befb775
Derrick Roccka <derrickroccka@gmail.com>
2014-03-13 11:41:12 +0100

    blog title changed to pink on hover and text decoration removed

--- application/views/partials/_blog.php
+++ application/views/partials/_blog.php
@@ -8,8 +8,8 @@ preg_match("'<h2 class=\"post-title\">(.*?)</h2>'", $html, $display);
 <section id="blog-container" class="section">
 	<?php echo partial('partials/_section-header', $data) ?>
 	<style>
-		.blog-title{text-decoration: none;}
-		.blog-title:hover{color: #cf7aab;}
+		.blog-title a{text-decoration: none;}
+		.blog-title:hover a{color: #cf7aab;}
 	</style>
 	<p style="font-size: 7rem;text-align:center; margin-top: 15%;color: #444;" class="blog-title"><?php echo $display[1];?></p>
 </section>
